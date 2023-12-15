706c1429c026d5c02c2865bf2b65a2786a0c69ea
GodFury <119965412+Vedansh564@users.noreply.github.com>
2023-12-15 11:00:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Welcome to Spotify");
 
 // Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio('songs2/Warriyo - Mortals (feat. Laura Brehm) [NCS Release].mp3');
+let audioElement = new Audio('songs3/Warriyo.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
@@ -10,16 +10,16 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs2/Warriyo - Mortals (feat. Laura Brehm) [NCS Release].mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs2/Huma-Huma-Crimson-Fly.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs2/Deaf Kev - Invincible Pt. II (feat. Sendi Hoxha) [NCS Release].mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs2/Different Heaven & EH!DE - My Heart [NCS Release].mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs2/Janji - Heroes Tonight (feat. Johnning) [NCS Release].mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs2/Ya Rabba (From Salaam-E-Ishq).mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs2/Salaam-E-Ishq - Salaam-E-Ishq 128 Kbps.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Pehle Bhi Main - Animal", filePath: "songs2/Pehle Bhi Main - Animal 128 Kbps.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Meet - Simran", filePath: "songs2/Meet - Simran 128 Kbps.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Tere Bina - Guru", filePath: "songs2/bollywood_Guru 2007 - Tere Bina.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs3/Warriyo.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Cielo - Huma-Huma", filePath: "songs3/HumaHuma.mp3", coverPath: "covers/2.jpg"},
+    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs3/Invincible.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs3/Different.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs3/Janji.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Rabba - Salam-e-Ishq", filePath: "songs3/Yarabba.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs3/Salaam.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Pehle Bhi Main - Animal", filePath: "songs3/Pehle.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Meet - Simran", filePath: "songs3/Meet.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Tere Bina - Guru", filePath: "songs3/terebina.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
